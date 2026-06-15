b4231ccad4f71a2bd0526365aff84322ae8fb576
CarEduCar <carloscarvalho201532@gmail.com>
2026-06-15 17:11:43 -0300

    feat: preparação inicial para criação adiante

--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { styles } from './Styles'
+import { View, Text } from 'react-native';
 
 export default function Quiz() {
 
+    
 
     return(
-        <View style={styles.teste}>
-
+        <View style={styles.container}>
+            <Text style={styles.texto}>
+                aoooooo
+            </Text>
         </View>
     );
 }
