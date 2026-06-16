def0b694e522c92847ef37cd2f7c975085f7e046
Vitor_ <vitor.silva@residente.serratec.org>
2026-06-16 18:15:15 -0300

    feat: Adição do componente header e estrutura inicial da página categoria

---
+++ src/pages/Categorias/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { styles } from './Styles'
+import { View, Text } from 'react-native';
+import { Button } from '../../components/Button/index'
+
+export default function Categorias() {
+
+    return(
+        <View style={styles.container}>
+
+        </View>
+    );
+}
