765f646720f13a26f6b45d3ddf05938f11c3ed07
Gabriel Wendling <gabrielwendling342@gmail.com>
2026-06-16 17:42:33 -0300

    adicionei a funcao de armazenamento e ja testei tudo certo depois vou tentar adicionar  contexto

--- src/pages/Ranking/index.tsx
+++ src/pages/Ranking/index.tsx
@@ -1,8 +1,63 @@
 import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { useEffect, useState } from "react";
 import RankingCard from "../../components/RankingCard";
-import { rankingMock } from "../../data/rankingMock";
+import { getRanking, addPlayer } from "../../services/rankingStorage";
+import { RankingPlayer } from "../../@types/ranking";
 
 export default function Ranking() {
+  const [ranking, setRanking] = useState<RankingPlayer[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        await addPlayer({
+  nome: "jonh quiz1",
+  categoria: "História",
+  acertos: 7,
+  totalPerguntas: 10,
+});
+
+await addPlayer({
+  nome: "Jonh quiz2",
+  categoria: "Geografia",
+  acertos: 5,
+  totalPerguntas: 10,
+});
+
+await addPlayer({
+  nome: "jonh quiz 3",
+  categoria: "Ciências",
+  acertos: 9,
+  totalPerguntas: 10,
+});
+
+        const data = await getRanking();
+
+        const ordered = data
+       .sort((a, b) => {
+       if (b.acertos !== a.acertos) {
+       return b.acertos - a.acertos;
+       }
+
+       return b.totalPerguntas - a.totalPerguntas;
+       })
+       .map((player, index) => ({
+       ...player,
+       posicao: index + 1,
+       }));
+
+        setRanking(ordered);
+      } catch (error) {
+        console.error("Erro ao carregar ranking:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadData();
+  }, []);
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.header}>
@@ -14,16 +69,23 @@ export default function Ranking() {
       </View>
 
       <View style={styles.rankingContainer}>
-        {rankingMock.map((player) => (
-          <RankingCard
-            key={player.posicao}
-            nome={player.nome}
-            categoria={player.categoria}
-            acertos={player.acertos}
-            totalPerguntas={player.totalPerguntas}
-            posicao={player.posicao}
-          />
-        ))}
+        {!loading && ranking.length === 0 && (
+          <Text style={styles.emptyText}>
+            Nenhum jogador no ranking ainda
+          </Text>
+        )}
+
+        {!loading &&
+          ranking.map((player) => (
+            <RankingCard
+              key={`${player.nome}-${player.categoria}`}
+              nome={player.nome}
+              categoria={player.categoria}
+              acertos={player.acertos}
+              totalPerguntas={player.totalPerguntas}
+              posicao={player.posicao}
+            />
+          ))}
       </View>
     </ScrollView>
   );
@@ -39,11 +101,9 @@ const styles = StyleSheet.create({
 
   header: {
     marginHorizontal: 15,
-
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-
     marginBottom: 35,
   },
 
@@ -56,9 +116,7 @@ const styles = StyleSheet.create({
   backButtonContainer: {
     width: 40,
     height: 40,
-
     backgroundColor: "#C51F48",
-
     justifyContent: "center",
     alignItems: "center",
   },
@@ -71,5 +129,12 @@ const styles = StyleSheet.create({
 
   rankingContainer: {
     paddingHorizontal: 20,
+  },
+
+  emptyText: {
+    color: "white",
+    textAlign: "center",
+    marginTop: 20,
+    fontSize: 16,
   },
 });
