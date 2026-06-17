98b721b70725ef80b5e49d992c95e4a61c04e888
CarEduCar <carloscarvalho201532@gmail.com>
2026-06-16 21:22:29 -0300

    feat: finalização do design da pagina quiz e adição de comentários no Card component

--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import { styles } from './Styles'
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView} from 'react-native';
 import { Button } from '../../components/Button/index'
+import { Card } from '../../components/Card/index'
 
 export default function Quiz() {
 
+    const tamButton = 47;
+    const raioButton = 20;
+
     return(
         <View style={styles.container}>
-            <Text style={styles.texto}>
-            </Text>
+            <View style={styles.card}>
+                <Card title='Questao' corCard='#C41E3A'/>
+            </View>
 
-            <Button title="teste"/>
+            <ScrollView style={styles.questao}>
+                <Text style={styles.enunciadoTexto}>
+                    
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London.
+                
+                </Text>
+            </ScrollView>
 
+            <View style={styles.respostas}>
+                <Button title="noruega" height={tamButton} raio={raioButton}/>
+
+                <Button title="italia" height={tamButton} raio={raioButton}/>
+
+                <Button title="amanha" height={tamButton} raio={raioButton}/>
+
+                <Button title="Estados Unidos" height={tamButton} raio={raioButton}/>
+            </View>
         </View>
     );
 }
