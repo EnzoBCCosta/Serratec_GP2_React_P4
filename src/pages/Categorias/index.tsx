aae61615bdc3bc5ca69218088e1de542ec0cbd61
Vitor_ <vitor.silva@residente.serratec.org>
2026-06-16 22:02:27 -0300

    feat: adição da página categorias

--- src/pages/Categorias/index.tsx
+++ src/pages/Categorias/index.tsx
@@ -1,13 +1,68 @@
-import React from 'react';
-import { styles } from './Styles'
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { styles } from './Styles';
+import { View, Text, FlatList } from 'react-native';
 import { Button } from '../../components/Button/index'
+import Header from '../../components/Header';
+
+interface Categoria {
+    id: number;
+    name: string;
+}
 
 export default function Categorias() {
 
+    const [categorias, setCategorias] = useState<Categoria[]>([]);
+
+    useEffect(() => {
+        async function carregarCategorias() {
+            try {
+                const response = await fetch(
+                    "https://tryvia.ptr.red/api_category.php"
+                );
+
+                const data = await response.json();
+
+                console.log(data);
+
+                setCategorias(data.trivia_categories);
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        carregarCategorias()
+    },[]);
+
     return(
         <View style={styles.container}>
+            <Header/>
+            <View style={styles.background}>
 
+                <View style={styles.titulo}>
+                    <Text style={styles.texto}>Escolha um tema:</Text>
+                </View>
+
+                <FlatList
+                    data={categorias}
+                    keyExtractor={(item) => item.id.toString()}
+                    contentContainerStyle={{
+                        alignItems: "center",
+                        paddingTop: 20,
+                        paddingBottom: 20,
+                        gap: 12,
+                    }}
+                    renderItem={({ item }) => (
+                        <Button
+                            title={item.name}
+                            widht={320}
+                            height={50}
+                            corButton='#f3ba2b'
+                        />
+                    )}
+                />
+
+            </View>
         </View>
     );
 }
